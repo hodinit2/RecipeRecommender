d145982fe56e4cc517b289a3442625e0f526b1b6
hodinit <horatiulazar2003@gmail.com>
2025-12-05 19:04:04 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecipeRecommender.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
